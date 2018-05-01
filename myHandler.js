c9eba7891d28a80ff1a68668d5557cbc20c16303
yeohwanyun <cwss1503@gmail.com>
2018-05-01 11:20:15 +0900

    first commit

--- myHandler.js
+++ myHandler.js
@@ -1,9 +1,20 @@
-function start() {
-    return "hello, Start!"
+function sleep(millisec){
+    let timeStart = new Date().getime;
+    while (new Data().getTime()< timeStart + millisec);
 }
 
-function hello() {
-    return "hello, Hello!"
+function Start(res) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('Hello,start!');
+      res.end();
+      return ;
+}
+
+function hello(res) {
+    sleep(20000);
+    res.write('Hello,start!');
+    res.end();
+    return ;
 }
 
 exports.start = start;
